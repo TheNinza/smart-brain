bd9772f3f7e73e86eca91c5ee49be0b17f9f292d
TheNinza <niks.a3198@gmail.com>
2020-04-30 13:48:57 +0530

    Updates

---
+++ precache-manifest.d3e07985950b15fa4cbe339012287ea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a501cc432051022e5a8cde946bf749",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "522b2a39c7da7eef1d1f",
+    "url": "/smart-brain/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "b21079da55786b283e31",
+    "url": "/smart-brain/static/css/main.64bbce05.chunk.css"
+  },
+  {
+    "revision": "522b2a39c7da7eef1d1f",
+    "url": "/smart-brain/static/js/2.f273aec0.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/smart-brain/static/js/2.f273aec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21079da55786b283e31",
+    "url": "/smart-brain/static/js/main.c11ec492.chunk.js"
+  },
+  {
+    "revision": "5631173ee026feeb2beb",
+    "url": "/smart-brain/static/js/runtime-main.2f2f904b.js"
+  }
+]);
